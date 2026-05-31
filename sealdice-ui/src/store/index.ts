22ea91b7f6c842c096d1f4f4ba7416b923ef6030
GuraQwQ <GuraQwQ@users.noreply.github.com>
2026-05-31 22:46:32 +0800

    重建 A 分支中文快照

---
+++ sealdice-ui/src/store/index.ts
@@ -0,0 +1,481 @@
+import { getCustomText, saveCustomText } from '~/api/configs';
+import {
+  getAdvancedConfig,
+  getDiceConfig,
+  setAdvancedConfig,
+  setDiceConfig,
+  type DiceConfig,
+} from '~/api/dice';
+import {
+  getConnectionList,
+  postAddDingtalk,
+  postAddDiscord,
+  postAddDodo,
+  postAddGocq,
+  postAddGocqSeparate,
+  postAddKook,
+  postAddLagrange,
+  postAddMilky,
+  postAddMilkyInternal,
+  postAddMinecraft,
+  postAddOfficialQQ,
+  postAddOnebot11ReverseWs,
+  postAddRed,
+  postAddSatori,
+  postaddSealChat,
+  postAddSlack,
+  postAddTelegram,
+} from '~/api/im_connections';
+import { getBaseInfo, getHello, getLogFetchAndClear, getPreInfo } from '~/api/others';
+import { getSalt, signin } from '~/api/signin';
+
+import type { addImConnectionForm } from '~/components/PageConnectInfoItems.vue';
+import type { AdvancedConfig } from '~/type.d.ts';
+import { toNumber } from 'lodash-es';
+export enum goCqHttpStateCode {
+  Init = 0,
+  InLogin = 1,
+  InLoginQrCode = 2,
+  InLoginBar = 3,
+  MilkyLoginConnected = 4,
+  MilkyLoginFailed = 5,
+  InLoginVerifyCode = 6,
+  InLoginDeviceLock = 7,
+  LoginSuccessed = 10,
+  LoginFailed = 11,
+  Closed = 20,
+}
+
+export const ImConnectionTypeGocqLegacy = 0;
+export const ImConnectionTypeDiscord = 1;
+export const ImConnectionTypeKook = 2;
+export const ImConnectionTypeTelegram = 3;
+export const ImConnectionTypeMinecraft = 4;
+export const ImConnectionTypeDodo = 5;
+export const ImConnectionTypeOnebotSeparate = 6;
+export const ImConnectionTypeRed = 7;
+export const ImConnectionTypeDingTalk = 8;
+export const ImConnectionTypeSlack = 9;
+export const ImConnectionTypeOfficialQQ = 10;
+export const ImConnectionTypeOnebotReverse = 11;
+// no 12 type here
+export const ImConnectionTypeSealChat = 13;
+export const ImConnectionTypeSatori = 14;
+export const ImConnectionTypeLagrangeOnebot = 15;
+// 16 is langrange gocq, deprecated
+export const ImConnectionTypeMilkySeparate = 17;
+export const ImConnectionTypeMilkyInternal = 18;
+
+export interface AdapterQQ {
+  DiceServing: boolean;
+  connectUrl: string;
+  curLoginFailedReason: string;
+  curLoginIndex: number;
+  loginState: goCqHttpStateCode;
+  inPackGoCqHttpLastRestricted: number;
+  inPackGoCqHttpProtocol: number;
+  inPackGoCqHttpAppVersion: string;
+  implementation: string;
+  useInPackGoCqhttp: boolean;
+  goCqHttpLoginVerifyCode: string;
+  goCqHttpLoginDeviceLockUrl: string;
+  ignoreFriendRequest: boolean;
+  goCqHttpSmsNumberTip: string;
+  useSignServer: boolean;
+  signServerConfig: any;
+  redVersion: string;
+  host: string;
+  port: number;
+  appID: number;
+  isReverse: boolean;
+  reverseAddr: string;
+  builtinMode: 'gocq' | 'lagrange' | 'lagrange-gocq';
+  built_in_mode: string; // Milky 的字段，跟 ob 不太一样
+  signServerVer: string;
+  signServerName: string;
+}
+
+interface TalkLogItem {
+  name?: string;
+  content: string;
+  isSeal?: boolean;
+  mode: 'private' | 'group';
+}
+
+export interface DiceConnection {
+  id: string;
+  state: number;
+  platform: string;
+  workDir: string;
+  enable: boolean;
+  protocolType: string;
+  nickname: string;
+  userId: number;
+  groupNum: number;
+  cmdExecutedNum: number;
+  cmdExecutedLastTime: number;
+  isPublic: boolean;
+
+  adapter: AdapterQQ;
+}
+
+export const urlPrefix = 'sd-api';
+
+interface DiceServer {
+  config: any;
+  customTextsHelpInfo: {
+    [k: string]: {
+      [k: string]: {
+        filename: string[];
+        origin: string[][];
+        vars: string[];
+        modified: boolean;
+        notBuiltin: boolean;
+        topOrder: number;
+        subType: string;
+        extraText: string;
+      };
+    };
+  };
+  customTexts: { [k: string]: { [k: string]: string[][] } };
+  previewInfo: {
+    [key: string]: {
+      version: string;
+      textV2: string;
+      textV1: string;
+      presetExists: boolean;
+      errV1: string;
+      errV2: string;
+    };
+  };
+  logs: { level: string; ts: number; caller: string; msg: string }[];
+  conns: DiceConnection[];
+  baseInfo: DiceBaseInfo;
+  qrcodes: { [key: string]: string };
+}
+
+interface DiceBaseInfo {
+  appChannel: string;
+  version: string;
+  versionSimple: string;
+  versionNew: string;
+  versionNewNote: string;
+  versionCode: number;
+  versionNewCode: number;
+  memoryAlloc: number;
+  memoryUsedSys: number;
+  uptime: number;
+  OS: string;
+  arch: string;
+  justForTest: boolean;
+  containerMode: boolean;
+}
+
+export type ResourceType = 'image' | 'audio' | 'video';
+
+export interface Resource {
+  type: ResourceType | 'unknown';
+  name: string;
+  ext: string;
+  path: string;
+  size: number;
+}
+
+export const useStore = defineStore('main', {
+  state: () => {
+    return {
+      salt: '',
+      token: '',
+      index: 0,
+      canAccess: false,
+      diceServers: [] as DiceServer[],
+
+      talkLogs: [
+        {
+          content:
+            '海豹已就绪。此界面可视为私聊窗口。\n设置中添加 Master 名为 UI:1001\n即可在此界面使用 master 命令！',
+          isSeal: true,
+          mode: 'private',
+        },
+        {
+          content:
+            '海豹已就绪。此界面可视为群聊窗口。\n设置中添加 Master 名为 UI:1002\n即可在此界面使用 master 命令！',
+          isSeal: true,
+          mode: 'group',
+        },
+        {
+          content: '（请注意，当前会话记录在刷新页面后会消失）',
+          isSeal: true,
+          mode: 'private',
+        },
+        {
+          content: '（请注意，当前会话记录在刷新页面后会消失）',
+          isSeal: true,
+          mode: 'group',
+        },
+      ] as TalkLogItem[],
+    };
+  },
+  getters: {
+    curDice(): DiceServer {
+      if (this.diceServers.length === 0) {
+        this.diceServers.push({
+          baseInfo: {
+            appChannel: 'stable',
+            version: '0.0',
+            versionSimple: '0.0',
+            versionNew: '0.0',
+            memoryUsedSys: 0,
+            memoryAlloc: 0,
+            uptime: 0,
+            versionNewNote: '',
+            versionCode: 0,
+            versionNewCode: 0,
+            OS: '',
+            arch: '',
+            justForTest: false,
+            containerMode: false,
+          },
+          customTexts: {},
+          customTextsHelpInfo: {},
+          previewInfo: {},
+          logs: [],
+          conns: [],
+          qrcodes: {},
+          config: {},
+        });
+      }
+
+      return this.diceServers[this.index];
+    },
+  },
+  actions: {
+    async customTextSave(category: string) {
+      await saveCustomText(category, this.curDice.customTexts[category]);
+    },
+
+    async getPreInfo() {
+      const info: {
+        testMode: boolean;
+      } = await getPreInfo();
+      return info;
+    },
+
+    async getBaseInfo() {
+      const info = await getBaseInfo();
+      if (!document.title.includes('-')) {
+        if (info.extraTitle && info.extraTitle !== '') {
+          document.title = `${info.extraTitle} - ${document.title}`;
+        }
+      }
+      this.curDice.baseInfo = info;
+      return info;
+    },
+
+    async getCustomText() {
+      const data = await getCustomText();
+      this.curDice.customTexts = data.texts;
+      this.curDice.customTextsHelpInfo = data.helpInfo;
+      this.curDice.previewInfo = data.previewInfo;
+      return data;
+    },
+
+    async getImConnections() {
+      const info = await getConnectionList();
+      this.diceServers[this.index].conns = info;
+      return info;
+    },
+
+    async addImConnection(form: addImConnectionForm) {
+      const {
+        accountType,
+        nickname,
+        account,
+        password,
+        protocol,
+        appVersion,
+        token,
+        botToken,
+        appToken,
+        proxyURL,
+        reverseProxyUrl,
+        reverseProxyCDNUrl,
+        url,
+        host,
+        port,
+        appID,
+        appSecret,
+        clientID,
+        robotCode,
+        implementation,
+        relWorkDir,
+        connectUrl,
+        accessToken,
+        useSignServer,
+        signServerConfig,
+        signServerName,
+        signServerVersion,
+        reverseAddr,
+        onlyQQGuild,
+        platform,
+        wsGateway,
+        restGateway,
+        builtInMode,
+      } = form;
+
+      let info = null;
+      switch (accountType) {
+        //QQ
+        case ImConnectionTypeGocqLegacy:
+          if (implementation === 'gocq') {
+            info = await postAddGocq(
+              account,
+              password,
+              protocol,
+              appVersion,
+              useSignServer,
+              signServerConfig,
+            );
+          }
+          // deprecated
+          // else if (implementation === 'walle-q') {
+          //   info = await postAddWalleQ(account, password, protocol);
+          // }
+          break;
+        case ImConnectionTypeDiscord:
+          info = await postAddDiscord(token.trim(), proxyURL, reverseProxyUrl, reverseProxyCDNUrl);
+          break;
+        case ImConnectionTypeKook:
+          info = await postAddKook(token.trim());
+          break;
+        case ImConnectionTypeTelegram:
+          info = await postAddTelegram(token.trim(), proxyURL);
+          break;
+        case ImConnectionTypeMinecraft:
+          info = await postAddMinecraft(url);
+          break;
+        case ImConnectionTypeDodo:
+          info = await postAddDodo(clientID.trim(), token.trim());
+          break;
+        case ImConnectionTypeOnebotSeparate: {
+          // onebot11 正向
+          let realUrl: string = connectUrl.trim();
+          if (!realUrl.startsWith('ws://') && !realUrl.startsWith('wss://')) {
+            realUrl = `ws://${realUrl}`;
+          }
+          info = await postAddGocqSeparate(relWorkDir, realUrl, accessToken, account);
+          break;
+        }
+        case ImConnectionTypeRed:
+          info = await postAddRed(host, port, token);
+          break;
+        case ImConnectionTypeDingTalk:
+          info = await postAddDingtalk(clientID, token, nickname, robotCode);
+          break;
+        case ImConnectionTypeSlack:
+          info = await postAddSlack(botToken, appToken);
+          break;
+        case ImConnectionTypeOfficialQQ:
+          info = await postAddOfficialQQ(Number(appID), appSecret, token, onlyQQGuild);
+          break;
+        case ImConnectionTypeOnebotReverse:
+          info = await postAddOnebot11ReverseWs(account, reverseAddr?.trim());
+          break;
+        case ImConnectionTypeSealChat:
+          info = await postaddSealChat(url.trim(), token.trim());
+          break;
+        case ImConnectionTypeSatori:
+          info = await postAddSatori(platform, host, port, token);
+          break;
+        case ImConnectionTypeLagrangeOnebot:
+          {
+            info = await postAddLagrange(account, signServerName, signServerVersion);
+          }
+          break;
+        // lagrange gocq deprecated
+        // case 16:
+        //   {
+        //     info = await postAddLagrange(account, signServerName, signServerVersion, true);
+        //   }
+        //   break;
+        case ImConnectionTypeMilkySeparate:
+          {
+            info = await postAddMilky(token, wsGateway, restGateway);
+          }
+          break;
+        case ImConnectionTypeMilkyInternal:
+          {
+            info = await postAddMilkyInternal(toNumber(account), builtInMode);
+          }
+          break;
+      }
+      return info as DiceConnection;
+    },
+    async logFetchAndClear() {
+      const info = await getLogFetchAndClear();
+      this.curDice.logs = info;
+    },
+
+    async diceConfigGet() {
+      const info = await getDiceConfig();
+      this.curDice.config = info;
+    },
+
+    async diceConfigSet(data: DiceConfig) {
+      await setDiceConfig(data);
+      if (data.uiPassword) {
+        window.location.reload();
+      }
+      await this.diceConfigGet();
+    },
+
+    async diceAdvancedConfigGet() {
+      const info: AdvancedConfig = await getAdvancedConfig();
+      return info;
+    },
+
+    async diceAdvancedConfigSet(data: AdvancedConfig) {
+      await setAdvancedConfig(data);
+      await this.diceAdvancedConfigGet();
+    },
+
+    // async toolOnebot() {
+    //   return await backend.post(
+    //     urlPrefix + '/tool/onebot',
+    //     undefined,
+    //     { headers: { token: this.token } }
+    //   ) as {
+    //     ok: boolean,
+    //     ip: string,
+    //     errText: string
+    //   }
+    // },
+
+    async signIn(password: string) {
+      try {
+        const ret = await signin(password);
+        const token = ret.token;
+        this.token = token;
+        localStorage.setItem('t', token);
+        this.canAccess = true;
+      } catch {
+        this.canAccess = false;
+      }
+    },
+    async trySignIn(): Promise<boolean> {
+      this.salt = (await getSalt()).salt;
+      const token = localStorage.getItem('t');
+      try {
+        await getHello();
+        this.token = token as string;
+        this.canAccess = true;
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (e) {
+        this.canAccess = false;
+        // 试图做一次登录，以获取 token
+        await this.signIn('defaultSignin');
+      }
+      return this.token != '';
+    },
+  },
+});
